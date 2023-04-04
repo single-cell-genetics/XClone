39940c3d52b5e1c0f9e062cc233d464bef284766
rthuang <rthuang@foxmail.com>
2023-04-04 23:08:17 +0800

    update GX109-T1c counts data

--- xclone/data/__init__.py
+++ xclone/data/__init__.py
@@ -6,5 +6,6 @@ from .demo_datasets._datasets import (
     tnbc1_baf,
     bch869_rdr,
     bch869_baf,
-    bch869_baf_50kb
+    bch869_baf_50kb,
+    gx109_rdr
 )
--- xclone/data/demo_datasets/_datasets.py
+++ xclone/data/demo_datasets/_datasets.py
@@ -6,10 +6,10 @@ from typing import Optional, Union
 import numpy as np
 import pandas as pd
 # import anndata as ad
-
 from scanpy import read
 # from scanpy import read, read_10x_h5, read_10x_mtx, write, read_visium
 
+## global url_datadir
 url_datadir = "https://github.com/Rongtingting/xclone-data/raw/main/"
 
 def tnbc1_rdr(file_path: Union[str, Path] = "data/TNBC1/TNBC1_RDR_adata_demo.h5ad"):
@@ -167,4 +167,81 @@ def bch869_baf_50kb(file_path: Union[str, Path] = "data/BCH869/BCH869_BAF_adata_
     url = f"{url_datadir}BCH869_scRNA/BCH869_BAF_adata_50kb.h5ad"
     adata = read(file_path, backup_url=url)
 
+    return adata
+
+from ...analysis.extract import dir_make
+import requests
+import zipfile
+import os
+
+def download_unzip_file(url, file_path, extract_path):
+    """
+    Example:
+    --------
+    url = "http://example.com/example.zip"
+    file_path = "/path/to/save/example.zip"
+    extract_path = "/path/to/extract"
+    download_unzip_file(url, file_path, extract_path)
+    """
+    # Download the ZIP file from the URL
+    response = requests.get(url)
+
+    # Save the ZIP file to the specified file path
+    with open(file_path, "wb") as f:
+        f.write(response.content)
+
+    # Extract the contents of the ZIP file to the specified extract path
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        zip_ref.extractall(extract_path)
+
+def gx109_rdr(file_path: Union[str, Path] = "/data/GX109-T1c/GX109-T1c_RDR_adata.zip",
+              extract_path: Union[str, Path] = "/data/GX109-T1c/",
+              raw_expr_layer: bool = True):
+    """Gastric cancer tissue.
+    scRNA-seq Source: GEOXXXX (will provide shortly)
+    Platform: 5' 10x Genomics (scRNA-seq)
+    Number of cells: 5400
+    Genome version: refdata-cellranger-GRCh38-3.0.0
+    cellranger: (v2.2.0)
+    Preprocessd: xcltk v0.1.15 RDR pipeline.
+    Notes: Released by XClone paper, there is a curated annotation 
+    that contains 5400 cells from 4 different cell types.
+
+    Arguments
+    ---------
+    file_path
+        Path where to save dataset and read it from.
+    extract_path
+        Path to extract zip files to h5ad file.
+    raw_expr_layer
+        bool, add raw_expr layer or not.
+    
+    Returns
+    -------
+    Returns `adata` object
+    
+    Example
+    -------
+    >>> import xclone
+    >>> RDR_adata = xclone.data.gx109_rdr()
+    """
+    h5ad_file = f"./data/GX109-T1c/GX109-T1c_RDR_adata.h5ad"
+    
+    if os.path.exists(h5ad_file):
+        print("load the GX109 rdr data from downloaded file.")
+    else:
+        print("load the GX109 rdr data from xclone-data github.")
+        url = f"{url_datadir}GX109-T1c_scRNA/GX109-T1c_RDR_adata.zip"
+        cwd = os.getcwd()
+        extract_path = f'{cwd}{extract_path}'
+        if os.path.exists(extract_path):
+            pass
+        else:
+            dir_make(extract_path)
+        file_path = f'{cwd}{file_path}'
+        download_unzip_file(url, file_path, extract_path)
+
+    adata = read(h5ad_file)
+    if raw_expr_layer:
+        adata.layers["raw_expr"] = adata.X.copy()
     return adata
