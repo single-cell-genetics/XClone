c175a894b5cb1bf3534169ca247fa5af63dd8bb4
rthuang <rthuang@foxmail.com>
2023-05-19 16:12:20 +0800

    BAF module minor update-threshold

--- xclone/model/xclone_baf_wrap.py
+++ xclone/model/xclone_baf_wrap.py
@@ -220,12 +220,11 @@ def run_BAF(BAF_adata,
     total_cell_num = merge_Xdata.obs.shape[0]
     try:
         ref_prop = ref_cell_num/total_cell_num
-        if  ref_prop <= 0.005:
+        if  ref_prop <= 0.01:
             theo_neutral_BAF = 0.5
+            print("[XClone hint] limited ref cells (%s), set theo_BAF as 0.5" % ref_prop)
     except Exception as e:
         print("[XClone Warning]", e)
-    else:
-        print("[XClone hint] limited ref cells (%s), set theo_BAF as 0.5" % ref_prop)
 
     if theo_neutral_BAF is not None:
         merge_Xdata.var["theo_neutral_BAF"] = theo_neutral_BAF
